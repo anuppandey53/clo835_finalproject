3fabbdf9ae40cf9a46d980cdc4bdce606a15654f
EC2 Default User <ec2-user@ip-172-31-5-79.ec2.internal>
2024-12-07 17:46:12 +0000

    workflow

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -1,49 +1,24 @@
-# Define the provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Adjust the region as needed
 }
 
-# Define tags locally
-locals {
-  default_tags = {
-    "env" = "production"  # Example environment tag
-    "Name" = "clo835-finalproject"
-  }
-}
-
-# ECR Repository for webapp
-resource "aws_ecr_repository" "webapp" {
-  name                 = "clo835-finalproject-webapp"
-  image_tag_mutability = "MUTABLE"
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "clo835-finalproject-webapp"
-    }
-  )
-}
-
-# ECR Repository for dbapp
+# Create ECR repository for DB
 resource "aws_ecr_repository" "dbapp" {
-  name                 = "clo835-finalproject-mysql"
-  image_tag_mutability = "MUTABLE"
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "clo835-finalproject-mysql"
-    }
-  )
+  name = "db-repository"
 }
 
-# EBS Volume for DB (gp2 type, 10 GB)
+# Create ECR repository for App
+resource "aws_ecr_repository" "webapp" {
+  name = "app-repository"
+}
+
+
+# Create an EBS volume with gp2 type and 16 GiB size
 resource "aws_ebs_volume" "db_volume" {
   availability_zone = "us-east-1a"  # Adjust the availability zone as needed
-  size              = 10            # Size of the volume in GiB (10 GB)
-  type              = "gp2"          # General Purpose SSD (gp2)
-
-  tags = merge(local.default_tags,
-    {
-      "Name" = "db-volume"
-    }
-  )
+  size              = 16            # Size updated to 16 GiB
+  type              = "gp2"          # General Purpose SSD
+  tags = {
+    Name = "db-volume"
+  }
 }
